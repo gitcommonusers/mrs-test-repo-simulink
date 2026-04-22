0df05db97c9d37667da9af7205c33a450b66d572
gitcommonusers <128743513+gitcommonusers@users.noreply.github.com>
2026-04-22 13:33:00 -0400

    Refactor buildfile.m for consistency and formatting

--- buildfile.m
+++ buildfile.m
@@ -1,15 +1,15 @@
 function plan = buildfile
-    plan = buildplan(localfunctions);
-    plan("test").Dependencies = "codeIssues";
-    plan.DefaultTasks = "test";
-end
+      plan = buildplan(localfunctions);
+      plan("test").Dependencies = "codeIssues";
+      plan.DefaultTasks = "test";
+  end
 
-function codeIssuesTask(~)
-    issues = codeIssues(pwd, IncludeSubfolders=true, FileTypes="code");
-    assert(issues.ErrorCount == 0, "Found %d code errors", issues.ErrorCount);
-end
+  function codeIssuesTask(~)
+      issues = codeIssues(pwd, IncludeSubfolders=true);
+      assert(issues.ErrorCount == 0, "Found %d code errors", issues.ErrorCount);
+  end
 
-function testTask(~)
-    results = runtests(pwd, IncludeSubfolders=true);
-    assertSuccess(results);
-end
+  function testTask(~)
+      results = runtests(pwd, IncludeSubfolders=true);
+      assertSuccess(results);
+  end
